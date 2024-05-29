fe306f8a237a8ea543c47d503ef82dfcb44ae43f
Kunal Bansal <98750058+KunalBansal12@users.noreply.github.com>
2024-05-29 20:17:52 +0530

    Time: 40 ms (67.27%) | Memory: 14.6 MB (97.19%) - LeetSync

--- 1284-four-divisors/four-divisors.cpp
+++ 1284-four-divisors/four-divisors.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     void divisor(int n, int &sum){
-        vector<int> arr;
+        int count=0,s=0;
         for(int i=1;i*i<=n;i++){
             if(n%i==0){
-                arr.push_back(i);
-                if(n/i!=i) arr.push_back(n/i);
+                count+=1,s+=i;
+                if(n/i!=i) count+=1,s+=(n/i);
             }
         }
-        if(arr.size()==4)
-            for(auto val:arr) sum += val;
+        if(count==4) sum+=s;
     }
     int sumFourDivisors(vector<int>& nums) {
         int sum = 0;
