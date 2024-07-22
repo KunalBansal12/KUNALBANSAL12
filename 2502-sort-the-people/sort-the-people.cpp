34c9c03139efc063cdfae9d7c7f172e346467422
Kunal Bansal <98750058+KunalBansal12@users.noreply.github.com>
2024-07-22 13:07:32 +0530

    Time: 33 ms (40.77%) | Memory: 26.5 MB (63.16%) - LeetSync

---
+++ 2502-sort-the-people/sort-the-people.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        unordered_map<int,string> map;
+        for(int i=0;i<names.size();i++){
+            map[heights[i]]=names[i];
+        }
+        sort(heights.begin(),heights.end());
+        vector<string> ans;
+        for(int i=heights.size()-1;i>=0;i--){
+            ans.push_back(map[heights[i]]);
+        }
+        return ans;
+    }
+};
