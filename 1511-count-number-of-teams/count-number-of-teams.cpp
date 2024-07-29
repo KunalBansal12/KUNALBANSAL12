1d4fc46fdabe315b742be70c6f3ed99920d2e4cc
Kunal Bansal <98750058+KunalBansal12@users.noreply.github.com>
2024-07-29 14:05:53 +0530

    Time: 31 ms (67.12%) | Memory: 11.6 MB (51.74%) - LeetSync

--- 1511-count-number-of-teams/count-number-of-teams.cpp
+++ 1511-count-number-of-teams/count-number-of-teams.cpp
@@ -1,15 +1,38 @@
 class Solution {
 public:
     int numTeams(vector<int>& rating) {
-        int ans=0;
-        for(int i=0;i<rating.size()-2;i++){
-            for(int j=i+1;j<rating.size()-1;j++){
-                for(int k=j+1;k<rating.size();k++){
-                    if(rating[i]>rating[j] && rating[j]>rating[k]) ans++;
-                    if(rating[i]<rating[j] && rating[j]<rating[k]) ans++;
+        int n = rating.size();
+        int ans = 0;
+        for(int i=1;i<n-1;++i){
+            int lsmall=0;int lhigh=0;int rsmall = 0;int rhigh = 0;
+            for(int j=0;j<i;++j){
+                if(rating[j]<rating[i]){
+                    ++lsmall;
+                }
+                else if(rating[j]>rating[i]){
+                    ++lhigh;
                 }
             }
+            for(int j=i+1;j<n;++j){
+                if(rating[j]<rating[i]){
+                    ++rsmall;
+                    }
+                else if(rating[j]>rating[i]){
+                    ++rhigh;
+                }
+            }
+            ans += ((lsmall*rhigh)+(lhigh*rsmall));
         }
         return ans;
+        // int ans=0;
+        // for(int i=0;i<rating.size()-2;i++){
+        //     for(int j=i+1;j<rating.size()-1;j++){
+        //         for(int k=j+1;k<rating.size();k++){
+        //             if(rating[i]>rating[j] && rating[j]>rating[k]) ans++;
+        //             if(rating[i]<rating[j] && rating[j]<rating[k]) ans++;
+        //         }
+        //     }
+        // }
+        // return ans;
     }
 };
