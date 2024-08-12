1b589968460c4fd159a65aa7cbd74674c3f08722
Kunal Bansal <98750058+KunalBansal12@users.noreply.github.com>
2024-08-12 18:38:50 +0530

    Time: 6 ms (15.08%) | Memory: 9 MB (22.51%) - LeetSync

--- 1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.cpp
+++ 1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     string smallestSubsequence(string s) {
-        unordered_map<int,int> map,index;
+        unordered_map<char,int> map;
+        vector<int> index(26,0);
         for(int i=0;i<s.length();i++){
             map[s[i]]++;
         }
         string st="";
         for(int i=0;i<s.length();i++){
-            if(index[s[i]]){
+            if(index[s[i]-'a']){
                 map[s[i]]--;
                 continue;
             }
             else{
-                index[s[i]]=i+1;
+                index[s[i]-'a']=i+1;
                 st+=s[i];
                 map[s[i]]--;
                 for(int j=st.length()-2;j>=0;j--){
-                    if(index[st[j]] && map[st[j]] && st[j]>s[i]){
-                        index[st[j]]=0;
+                    if(index[st[j]-'a'] && map[st[j]] && st[j]>s[i]){
+                        index[st[j]-'a']=0;
                     }
-                    else if(index[st[j]] && !map[st[j]]) break;
+                    else if(index[st[j]-'a'] && !map[st[j]]) break;
                 }
             }
         }
