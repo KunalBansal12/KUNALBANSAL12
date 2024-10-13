7406299114de5960dfecfeac1917a283cdfc510e
Kunal Bansal <98750058+KunalBansal12@users.noreply.github.com>
2024-10-13 16:38:00 +0530

    Time: 42 ms (81.37%) | Memory: 48.5 MB (36.37%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
@@ -2,26 +2,11 @@ class Node{
     public:
     Node* links[26];
     bool flag=false;
-    bool containsKey(char c){
-        return links[c-'a']!=NULL;
-    }
-    void create(char c,Node* n){
-        links[c-'a']=n;
-    }
-    Node* get(char c){
-        return links[c-'a'];
-    }
-    void setEnd(){
-        flag=true;
-    }
-    bool isEnd(){
-        return flag==true;
-    }
 };
 
 class Trie {
-    Node* root;
 public:
+    Node* root;
     Trie() {
         root=new Node();
     }
@@ -29,32 +14,91 @@ public:
     void insert(string word) {
         Node* temp=root;
         for(int i=0;i<word.length();i++){
-            if(!temp->containsKey(word[i])){
-                temp->create(word[i],new Node());
+            if(!temp->links[word[i]-'a']){
+                temp->links[word[i]-'a']=new Node();
             }
-            temp=temp->get(word[i]);
+            temp=temp->links[word[i]-'a'];
         }
-        temp->setEnd();
+        temp->flag=true;
+        return;
     }
     
     bool search(string word) {
         Node* temp=root;
         for(int i=0;i<word.length();i++){
-            if(!temp->containsKey(word[i])) return false;
-            temp=temp->get(word[i]);
+            if(!temp->links[word[i]-'a']) return false;
+            temp=temp->links[word[i]-'a'];
         }
-        return temp->isEnd();
+        return temp->flag==true;
     }
     
     bool startsWith(string prefix) {
         Node* temp=root;
         for(int i=0;i<prefix.length();i++){
-            if(!temp->containsKey(prefix[i])) return false;
-            temp=temp->get(prefix[i]);
+            if(!temp->links[prefix[i]-'a']) return false;
+            temp=temp->links[prefix[i]-'a'];
         }
         return true;
     }
 };
+
+//  class Node{
+//     public:
+//     Node* links[26];
+//     bool flag=false;
+//     bool containsKey(char c){
+//         return links[c-'a']!=NULL;
+//     }
+//     void create(char c,Node* n){
+//         links[c-'a']=n;
+//     }
+//     Node* get(char c){
+//         return links[c-'a'];
+//     }
+//     void setEnd(){
+//         flag=true;
+//     }
+//     bool isEnd(){
+//         return flag==true;
+//     }
+// };
+
+// class Trie {
+//     Node* root;
+// public:
+//     Trie() {
+//         root=new Node();
+//     }
+    
+//     void insert(string word) {
+//         Node* temp=root;
+//         for(int i=0;i<word.length();i++){
+//             if(!temp->containsKey(word[i])){
+//                 temp->create(word[i],new Node());
+//             }
+//             temp=temp->get(word[i]);
+//         }
+//         temp->setEnd();
+//     }
+    
+//     bool search(string word) {
+//         Node* temp=root;
+//         for(int i=0;i<word.length();i++){
+//             if(!temp->containsKey(word[i])) return false;
+//             temp=temp->get(word[i]);
+//         }
+//         return temp->isEnd();
+//     }
+    
+//     bool startsWith(string prefix) {
+//         Node* temp=root;
+//         for(int i=0;i<prefix.length();i++){
+//             if(!temp->containsKey(prefix[i])) return false;
+//             temp=temp->get(prefix[i]);
+//         }
+//         return true;
+//     }
+// };
 
 /**
  * Your Trie object will be instantiated and called as such:
