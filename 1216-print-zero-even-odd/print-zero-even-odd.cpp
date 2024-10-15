cc1d886316e32f27826cdbcf902b606adbbba99e
Kunal Bansal <98750058+KunalBansal12@users.noreply.github.com>
2024-10-15 16:08:07 +0530

    Time: 4 ms (55.51%) | Memory: 9.3 MB (85.01%) - LeetSync

--- 1216-print-zero-even-odd/print-zero-even-odd.cpp
+++ 1216-print-zero-even-odd/print-zero-even-odd.cpp
@@ -1,6 +1,55 @@
 class ZeroEvenOdd {
 private:
     int n;
+    int state;
+    int curr;
+    condition_variable cv;
+    mutex m1;
+public:
+    ZeroEvenOdd(int n) {
+        this->n = n;
+        state=0;
+        curr=1;
+    }
+
+    // printNumber(x) outputs "x", where x is an integer.
+    void zero(function<void(int)> printNumber) {
+        for (int i = 1; i <= n; i++) {
+            unique_lock<mutex> lock(m1);
+            cv.wait(lock,[&]{return state==0;});
+            printNumber(0);
+            state=(curr%2 ? 1:2);
+            cv.notify_all();
+        }
+    }
+
+    void even(function<void(int)> printNumber) {
+        for (int i = 2; i <= n; i+= 2) {
+            unique_lock<mutex> lock(m1);
+            cv.wait(lock,[&]{return state==2;});
+            printNumber(i);
+            curr++;
+            state=0;
+            cv.notify_all();
+        }
+    }
+
+    void odd(function<void(int)> printNumber) {
+        for (int i = 1; i <= n; i+= 2) {
+            unique_lock<mutex> lock(m1);
+            cv.wait(lock,[&]{return state==1;});
+            printNumber(i);
+            curr++;
+            state=0;
+            cv.notify_all();
+        }
+    }
+};
+
+/*
+class ZeroEvenOdd {
+private:
+    int n;
     mutex m1,m2,m3;
 public:
     ZeroEvenOdd(int n) {
@@ -37,3 +86,4 @@ public:
         }
     }
 };
+*/
