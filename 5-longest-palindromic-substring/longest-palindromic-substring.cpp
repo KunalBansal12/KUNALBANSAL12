7c2ce25665d0ad7ecf69c93ab8c3b78b0874b9a5
Kunal Bansal <98750058+KunalBansal12@users.noreply.github.com>
2024-10-18 12:48:51 +0530

    Time: 1996 ms (5.00%) | Memory: 532.9 MB (5.36%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -18,25 +18,6 @@ public:
                 ans=len;
                 ans1=a;
             }
-            if(k>0 && s[k-1]==s[k]){
-                len=2;
-                i=k-2;
-                j=k+1;
-                a="";
-                a=a+s[k-1];
-                a=a+s[k];
-                while(i>=0 && j<n && s[i]==s[j]){
-                    a=s[i]+a;
-                    a=a+s[j];
-                    i--;
-                    j++;
-                    len+=2;
-                }
-                if(ans<len){
-                    ans=len;
-                    ans1=a;
-                }
-            }
             if(k<n-1 && s[k+1]==s[k]){
                 len=2;
                 i=k-1;
