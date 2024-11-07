b28a0b04ed868dac799d4437d11791d3fe98f705
Kunal Bansal <98750058+KunalBansal12@users.noreply.github.com>
2024-11-07 11:42:06 +0530

    Time: 49 ms (11.19%) | Memory: 60.3 MB (14.16%) - LeetSync

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.cpp
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     int largestCombination(vector<int>& nums) {
         int n=nums.size();
+        int ans=0;
         vector<int> bits(24,0);
         for(int i=0;i<n;i++){
             for(int j=0;j<24;j++){
                 bits[j]+=nums[i]%2;
                 nums[i]=nums[i]/2;
+                if(i==n-1) ans=max(ans,bits[j]);
             }
         }
-        int ans=0;
-        for(int i=0;i<24;i++) ans=max(ans,bits[i]);
+        // for(int i=0;i<24;i++) ans=max(ans,bits[i]);
         return ans;
     }
 };
