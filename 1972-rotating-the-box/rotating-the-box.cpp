48d356feead2e57fef49e47856e34948040efbde
Kunal Bansal <98750058+KunalBansal12@users.noreply.github.com>
2024-11-23 15:43:48 +0530

    Time: 192 ms (60.00%) | Memory: 56.4 MB (25.37%) - LeetSync

---
+++ 1972-rotating-the-box/rotating-the-box.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
+        int n=box.size(),m=box[0].size();
+        vector<vector<char>> ans(m,vector<char>(n,'.'));
+        for(int i=0;i<n;i++){
+            int k=m-1;
+            for(int j=m-1;j>=0;j--){
+                if(box[i][j]=='.') continue;
+                else if(box[i][j]=='*'){
+                    k=j-1;
+                    ans[j][n-i-1]='*';
+                }
+                else if(box[i][j]=='#'){
+                    ans[k][n-i-1]='#';
+                    k--;
+                }
+            }
+        }
+        return ans;
+    }
+};
